1aab3781b0901e3e99d79e20e986e514bae14057
Patil Kaustubh <patilkaustubh990@gmail.com>
2024-04-24 01:31:44 +0530

    Mereged landingpage and chatapp. Add project description and tech stack information to README.md

---
+++ ChatApp/app/hooks/useRoutes.ts
@@ -0,0 +1,44 @@
+import { useState, useMemo } from "react";
+import { usePathname } from "next/navigation";
+import { HiChat } from 'react-icons/hi';
+import { HiArrowLeftOnRectangle, HiUsers } from 'react-icons/hi2';
+import { signOut } from "next-auth/react";
+import useConversation from "./useConversation";
+import Loading from '../conversations/loading'; // import your loading component
+
+const useRoutes = () => {
+  const pathname = usePathname();
+  const { conversationId } = useConversation();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSignOut = async () => {
+    setIsLoading(true);
+    await signOut();
+    setIsLoading(false);
+  };
+
+  const routes = useMemo(() => [
+    { 
+      label: 'Chat', 
+      href: '/conversations', 
+      icon: HiChat,
+      active: pathname === '/conversations' || !!conversationId
+    },
+    // { 
+    //   label: 'Members ', 
+    //   href: '/users', 
+    //   icon: HiUsers, 
+    //   active: pathname === '/users'
+    // },
+    {
+      label: isLoading ? 'Loading...' : 'Logout', 
+      onClick: handleSignOut,
+      href: '#',
+      icon: isLoading ? Loading : HiArrowLeftOnRectangle, 
+    }
+  ], [pathname, conversationId, isLoading]);
+
+  return routes;
+};
+
+export default useRoutes;
---
+++ ChatApp/app/layout.tsx
@@ -0,0 +1,27 @@
+import './globals.css'
+import AuthContext from './context/AuthContext'
+import ActiveStatus from './components/ActiveStatus'
+import ToasterContext from './context/ToasterContext'
+
+export const metadata = {
+  title: 'Chatrooms-StrayAidConnect',
+  description: 'A chatroom application for StrayAidConnect users'
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <AuthContext>
+          <ToasterContext />
+          <ActiveStatus />
+          {children}
+        </AuthContext>
+      </body>
+    </html>
+  )
+}
